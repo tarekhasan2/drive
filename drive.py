9d98b0a77e8ad57b995f11070a3d5dadbc00797b
tarekhasan2 <hasan867656@gmail.com>
2023-02-20 01:47:36 +0600

    add created date and download link

--- drive.py
+++ drive.py
@@ -5,40 +5,49 @@ from Google import Create_Service
 def get_values_from_files(files):
 	indexs 				= ('#',)
 	video_names  		= ('Title',)
+	video_created_dates = ('Created date',) 		
 	video_links 		= ('Google Drive Link',)
-	video_embed_codes 	= ('Embed Code',) 
-	for index, file in enumerate(files):
-		video_id 	= file.get('id')
-		name 		= file.get('name')
-		link 		= f"https://drive.google.com/file/d/{video_id}/view"
-		embed_code 	= f"<iframe src='https://drive.google.com/file/d/{video_id}/preview' width='640' height='480' allow='autoplay'></iframe>"
+	video_embed_codes 	= ('Embed Code',)
+	video_download_links= ('Download Link',)
 
-		indexs    		= indexs + (index, )
-		video_names		= video_names + (name,) 
-		video_links 	= video_links + (link,)
-		video_embed_codes= video_embed_codes + (embed_code,)
+	for index, file in enumerate(files):
+		video_id 		= file.get('id')
+		name 			= file.get('name')
+		created_date 	= file.get('createdTime')
+		link 			= f"https://drive.google.com/file/d/{video_id}/view"
+		embed_code 		= f"<iframe src='https://drive.google.com/file/d/{video_id}/preview' width='640' height='480' allow='autoplay'></iframe>"
+		download_link 	= file.get('webContentLink')
+
+		indexs    			= indexs + (index, )
+		video_names			= video_names + (name,) 
+		video_created_dates	= video_created_dates + (created_date,)
+		video_links 		= video_links + (link,)
+		video_embed_codes	= video_embed_codes + (embed_code,)
+		video_download_links= video_download_links + (download_link,)
 
 	print(video_names)
+	print('\n')
+	print(video_created_dates)
+	print('\n')
 	print(video_links)
+	print('\n')
 	print(video_embed_codes)
-	return indexs, video_names, video_links, video_embed_codes
+	print('\n')
+	print(video_download_links)
+	print('\n\n')
+
+	return indexs, video_names, video_created_dates, video_links, video_embed_codes, video_download_links
 
 
-def google_drive():
+def google_drive(folder_id):
 	CLIENT_SECRET_FILE 	= 'credentials.json'
 	API_NAME 			= 'drive'
 	API_VERSION 		= 'v3'
 	SCOPES 				= ['https://www.googleapis.com/auth/drive']
 	drive_service 		= Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-	
-	folder_id 	= ''
-	'''This will the folder id of the folder where all the videos are in. You will get the folder id in folder link.
-		Like this, folder link: https://drive.google.com/drive/folders/1fGZpA7FzlQj6yy5LlJlw2PMpTuFekyKV
-		folder_id = 1fGZpA7FzlQj6yy5LlJlw2PMpTuFekyKV
-		put the folder_id in the field.
-	'''
 	query 		= f"parents = '{folder_id}'"
-	response 	= drive_service.files().list(q = query).execute()
+
+	response 	= drive_service.files().list(q = query, fields= "files(id, name, createdTime, webContentLink)").execute()
 	
 	files 			= response.get('files')
 	next_page_token = response.get('nextPageToken')
@@ -52,8 +61,7 @@ def google_drive():
 
 
 def google_sheet(files):
-	indexs, video_names, video_links, video_embed_codes = get_values_from_files(files)
-
+	indexs, video_names, video_created_dates, video_links, video_embed_codes, video_download_links = get_values_from_files(files)
 
 	CLIENT_SECRET_FILE 	= 'credentials.json'
 	API_NAME 			= 'sheets'
@@ -76,10 +84,12 @@ def google_sheet(files):
 	worksheet_name = 'sheet1'
 
 	values = (
-		indexs,
-		video_names,
-		video_links,
-		video_embed_codes
+		indexs, 
+		video_names, 
+		video_created_dates, 
+		video_links, 
+		video_embed_codes, 
+		video_download_links
 		)
 	value_range_body  = {
 		'majorDimension': 'COLUMNS',
@@ -97,7 +107,9 @@ def google_sheet(files):
 
 	print('DONE... All data saved in spreadsheet. URL: {}'.format(sheet.get('spreadsheetUrl')))
 
-files = google_drive()
+
+folder_id = input('Enter The Folder ID: ')
+files = google_drive(folder_id)
 google_sheet(files)
 
 
